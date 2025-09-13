f83f422c8729be0e64882e84ed0a7154b83c27f1
Tolorunju Tayo James <jamescoding93@gmail.com>
2025-09-13 16:07:40 +0100

    implemented profile, sign out and in

---
+++ components/navbar/UserIcon.tsx
@@ -0,0 +1,17 @@
+import { LuUser } from "react-icons/lu"
+import { fetchProfileImage } from "@/utils/actions"
+
+async function UserIcon() {
+  const profileImage = await fetchProfileImage()
+  if (profileImage) {
+    return(
+      <img src={profileImage} className="h-6 w-6 rounded-full object-cover" />
+    )
+  }
+  return (
+    <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />
+
+  )
+}
+
+export default UserIcon
---
+++ utils/actions.ts
@@ -0,0 +1,112 @@
+'use server'
+
+import { imageSchema, profileSchema, validatedWithZodSchema } from "./schemas";
+import db from './db';
+import {  clerkClient, currentUser } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const getAuthUser = async ()=>{
+    const user = await currentUser();
+    if (!user) {
+        throw new Error('You must be logged in to access this route');
+    }
+    if (!user.privateMetadata.hasProfile) redirect('/')
+        return user 
+}
+
+const renderError = (error: unknown): { message: string} =>{
+    console.log(error)
+    return { message: error instanceof Error ? error.message : 'An error has occurred' }
+}
+export const createProfileAction = async (
+    prevState: unknown, 
+    formData: FormData
+) => {
+    try {
+        const user = await currentUser();
+        if (!user) throw new Error('Please login to create a profile');
+
+        const rawData = Object.fromEntries(formData);
+       const validatedFields = validatedWithZodSchema(profileSchema, rawData)
+
+        await db.profile.create({
+            data: {
+                clerkId: user.id,
+                email: user.emailAddresses[0].emailAddress,
+                profileImage: user.imageUrl ?? '',
+                ...validatedFields,
+            }
+        })
+        await clerkClient.users.updateUserMetadata(user.id, {
+            privateMetadata: {
+                hasProfile: true,
+            }
+        })
+
+    } catch (error) {
+        return renderError(error)
+    }
+    redirect('/')
+}
+
+export const fetchProfileImage = async() => {
+    const user = await currentUser();
+    if (!user) return null;
+
+    const profile = await db.profile.findUnique({
+        where: {
+            clerkId: user.id,
+        },
+        select: {
+            profileImage: true,
+        },
+    });
+
+    return profile?.profileImage
+}
+
+export const fetchProfile = async () =>{
+    const user = await getAuthUser()
+    const profile = await db.profile.findUnique({
+        where: {
+            clerkId: user.id,
+        },
+    })
+    if (!profile) redirect('/profile/create')
+        return profile
+}
+
+export const updateProfileAction = async(
+    prevState: unknown,
+    formData: FormData
+): Promise<{message: string}>=>{
+    const user = await getAuthUser();
+
+    try {
+        const rawData = Object.fromEntries(formData);
+       const validatedFields = validatedWithZodSchema(profileSchema, rawData)
+
+        await db.profile.update({
+            where: {
+                clerkId: user.id,
+            },
+            data: validatedFields,
+        })
+        revalidatePath('/profile');
+        return { message: 'Profile updated successfully'}
+    } catch (error) {
+        return renderError(error)
+    }
+    
+}
+
+export const updateProfileImageAction = async (
+     prevState: unknown,
+    formData: FormData
+): Promise<{ message: string }> => {
+    const image = formData.get('image') as File;
+    const validatedField = validatedWithZodSchema(imageSchema, {image});
+    console.log(validatedField);
+  return { message: 'Profile image updated successfully' };
+};
