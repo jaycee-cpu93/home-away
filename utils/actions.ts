0d1daac16bfee9ead25715c53af77fb6da910463
Tolorunju Tayo James <jamescoding93@gmail.com>
2025-11-07 19:50:13 +0100

    implemented the home, favorite, create rentals, profile

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,10 +1,11 @@
 'use server'
 
-import { imageSchema, profileSchema, validatedWithZodSchema } from "./schemas";
+import { imageSchema, profileSchema, propertySchema, validatedWithZodSchema } from "./schemas";
 import db from './db';
 import {  clerkClient, currentUser } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { uploadImage } from "./supabase";
 
 const getAuthUser = async ()=>{
     const user = await currentUser();
@@ -103,10 +104,158 @@ export const updateProfileAction = async(
 
 export const updateProfileImageAction = async (
      prevState: unknown,
-    formData: FormData
+     formData: FormData
 ): Promise<{ message: string }> => {
-    const image = formData.get('image') as File;
-    const validatedField = validatedWithZodSchema(imageSchema, {image});
-    console.log(validatedField);
-  return { message: 'Profile image updated successfully' };
+    const user = await getAuthUser();
+    try {
+         const image = formData.get('image') as File;
+         const validatedFields = validatedWithZodSchema(imageSchema, {image});
+         const fullPath = await uploadImage(validatedFields.image);
+            
+         await db.profile.update({
+            where: {
+                clerkId: user.id,
+            },
+            data: {
+                profileImage: fullPath,
+            },
+         });
+         revalidatePath('/profile');
+         return { message: 'Profile image updated successfully' };
+    } catch (error) {
+        return renderError(error)
+    }
+   
+
 };
+
+export const createPropertyAction = async (
+     prevState: unknown,
+     formData: FormData
+): Promise<{ message: string }> => {
+    const user = await getAuthUser();
+    console.log(user)
+
+    try {
+        const rawData = Object.fromEntries(formData);
+        const file = formData.get('image') as File;
+        const validatedFields = validatedWithZodSchema(propertySchema, rawData)
+        const validateFile = validatedWithZodSchema(imageSchema, {image: file})
+        const fullPath = await uploadImage(validateFile.image)
+
+        await db.property.create({
+            data: {
+                ...validatedFields,
+                image: fullPath,
+                profileId: user.id,
+            },
+        });
+    } catch (error) {
+        return renderError(error)
+    }
+    redirect('/')
+
+}
+
+export const fetchProperties = async({search='', category}: {search?: string, category?:string})=>{
+    const properties = await db.property.findMany({
+        where: {
+            category,
+            OR: [
+                {name: {contains: search, mode: 'insensitive'}},
+                {tagline: {contains: search, mode: 'insensitive'}}
+            ]
+        },
+        select: {
+            id: true,
+            name: true,
+            image: true,
+            tagline: true,
+            country: true,
+            price: true,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        }
+    });
+    return properties
+}
+
+export const fetchFavoriteId = async({propertyId}: {propertyId: string}) => {
+    const user = await getAuthUser()
+    const favorite = await db.favorite.findFirst({
+        where:{
+            propertyId,
+            profileId: user.id
+        },
+        select: {
+            id: true
+        }
+    });
+    return favorite?.id || null
+}
+
+export const toggleFavoriteAction = async(prevState: {
+     propertyId: string;
+    favoriteId: string | null;
+    pathname: string 
+})=>{
+    const user = await getAuthUser()
+   const {propertyId, favoriteId, pathname} = prevState;
+    try {
+        if (favoriteId) {
+            await db.favorite.delete({
+                where: {
+                    id: favoriteId
+                }
+            })
+        }
+        else{
+            await db.favorite.create({
+                data: {
+                    propertyId, 
+                    profileId: user.id,
+                }
+            })
+        }
+        revalidatePath(pathname);
+        return { message: favoriteId ? 'Remove from Faves' : 'Added to Faves'};
+    } catch (error) {
+        return renderError(error);
+    }
+}
+
+export const fetchFavorites = async()=>{
+    const user = await getAuthUser()
+    const favorites = await db.favorite.findMany({
+        where: {
+            profileId: user.id
+        },
+         select: {
+            property: {
+                select: {
+                    id: true,
+                    name: true,
+                    image: true,
+                    tagline: true,
+                    country: true,
+                    price: true,
+                }
+            }
+        },
+
+    });
+    return favorites.map((favorite)=> favorite.property)
+};
+
+
+export const fetchPropertyDetails = (id: string) =>{
+    return db.property.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            profile: true,
+        }
+    })
+} 
